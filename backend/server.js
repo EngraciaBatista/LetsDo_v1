d918cb6e7c30022bc8f597f66db36d21b2ac8d6a
Engracia Batista <engraciadacosta@gmail.com>
2024-11-23 00:03:16 -0500

    Backup_Upload_V1_of_the_code_EngraciaBatista

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users-routes");
+const tasksRoutes = require("./routes/tasks-routes"); 
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/users", usersRoutes);
+app.use("/api/tasks", tasksRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+mongoose.connect("mongodb+srv://gail:255650@cluster0.qod2v.mongodb.net/LetsDo?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server is running on http://localhost:8080");
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
