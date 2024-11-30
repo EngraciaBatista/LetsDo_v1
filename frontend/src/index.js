7a64e8b76b2a0a056b5bf4bfb38d48891b7de69b
Benjamin3k <bserwadd@my.centennialcollege.ca>
2024-11-29 19:51:04 -0600

    I had to install the react-router-dom, apply changes to that the landing page routes to the task page which i am still in process of ironing out. The landing does go to tasks, but brings a blank page. I also added better functionality for sign ups and sign ins. filles affected by the router dom addition are the package-lock.json, the package.json, index.js. style and funcitonality has affected index.css, and landingpage.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,25 @@
+//Hey team, this page is what I have used to Route the landing to the tasks page. 
+//I had to make the render a case switch that starts on landing and ends on tasks page.
+//After which, I worked on the functionality of log ins and signups taking users directly to the tasks page. -Benjamin Serwadda
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Landing from './LandingPage';
-
+import TaskManager from './tasks/Task';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing />
+   <Router>
+    <Routes>
+     <Route path="/" element={<Landing />}/>
+     <Route path="/tasks" element={<TaskManager/>}/>
     {/*<App />*/}
+    </Routes>
+  </Router>
   </React.StrictMode>
 );
 
