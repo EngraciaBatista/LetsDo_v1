b2d32f13bbc3b91c8b613300dda6daaf4a781242
Engracia Batista <engraciadacosta@gmail.com>
2024-12-04 00:35:26 -0500

    Engracia: Fixed App loading - now loading from tasks only

--- frontend/src/tasks/Task.js
+++ frontend/src/tasks/Task.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './Task.css';
+import Footer from '../shared/Footer';
 import { getTasks, createTask, updateTask, deleteTask } from '../services/taskService';
 
 function TaskManager() {
   const [task, setTask] = useState('');
-  const [category, setCategory] = useState('Personal'); // Default category
-  const [priority, setPriority] = useState('Medium'); // Default priority
+  const [category, setCategory] = useState('Personal');
+  const [priority, setPriority] = useState('Medium');
   const [taskList, setTaskList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [currentTaskId, setCurrentTaskId] = useState(null);
@@ -24,30 +25,16 @@ function TaskManager() {
   };
 
   const handleAddTask = async () => {
-    if (task.trim() !== '') {
+    if (task.trim()) {
       try {
-        const newTask = {
-          title: task,
-          status: false,
-          category, // Include category
-          priority, // Include priority
-          user: 'placeholderUserId', // Update this once authentication is implemented
-        };
+        const newTask = { title: task, category, priority, user: 'placeholderUserId' };
         const addedTask = await createTask(newTask);
         setTaskList([...taskList, addedTask]);
-        setTask('');
-        setCategory('Personal'); // Reset to default
-        setPriority('Medium'); // Reset to default
+        resetForm();
       } catch (error) {
         console.error('Error adding task:', error);
       }
     }
-  };
-
-  const handleEditTask = (id, currentTaskTitle) => {
-    setTask(currentTaskTitle);
-    setIsEditing(true);
-    setCurrentTaskId(id);
   };
 
   const handleUpdateTask = async () => {
@@ -55,11 +42,7 @@ function TaskManager() {
       const updatedTask = { title: task, category, priority };
       await updateTask(currentTaskId, updatedTask);
       fetchTasks();
-      setTask('');
-      setCategory('Personal'); // Reset to default
-      setPriority('Medium'); // Reset to default
-      setIsEditing(false);
-      setCurrentTaskId(null);
+      resetForm();
     } catch (error) {
       console.error('Error updating task:', error);
     }
@@ -74,62 +57,84 @@ function TaskManager() {
     }
   };
 
+  const resetForm = () => {
+    setTask('');
+    setCategory('Personal');
+    setPriority('Medium');
+    setIsEditing(false);
+    setCurrentTaskId(null);
+  };
+
   return (
-    <div className="task-manager">
-      <div className="input-container">
-        <input
-          type="text"
-          placeholder="Enter a task"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="Personal">Personal</option>
-          <option value="Studies">Studies</option>
-          <option value="Work">Work</option>
-          <option value="Other">Other</option>
-        </select>
-        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
-          <option value="Low">Low</option>
-          <option value="Medium">Medium</option>
-          <option value="High">High</option>
-        </select>
-        {isEditing ? (
-          <button className="update-button" onClick={handleUpdateTask}>
-            Update
-          </button>
-        ) : (
-          <button className="add-button" onClick={handleAddTask}>
-            Add
-          </button>
-        )}
-      </div>
-      <div className="task-container">
-        {taskList.length > 0 ? (
-          <ul>
-            {taskList.map(({ _id, title, category, priority }) => (
-              <li key={_id}>
-                <span>
-                  {title} - <b>{category}</b> - <i>{priority}</i>
-                </span>
-                <button
-                  className="edit-button"
-                  onClick={() => handleEditTask(_id, title)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="delete-button"
-                  onClick={() => handleDeleteTask(_id)}
-                >
-                  Delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No tasks available</p>
-        )}
+    <div className="App">
+      <div className="app-container">
+        <img src="/letsdologo_header.png" alt="Logo" className="logo" />
+        <h2>Task Manager</h2>
+        <p>Welcome to LetsDo, your personal task manager.</p>
+        <div className="task-manager">
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Enter a task"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+              <option value="Personal">Personal</option>
+              <option value="Studies">Studies</option>
+              <option value="Work">Work</option>
+              <option value="Other">Other</option>
+            </select>
+            <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+            {isEditing ? (
+              <button className="update-button" onClick={handleUpdateTask}>
+                Update
+              </button>
+            ) : (
+              <button className="add-button" onClick={handleAddTask}>
+                Add
+              </button>
+            )}
+          </div>
+          <div className="task-container">
+            {taskList.length > 0 ? (
+              <ul>
+                {taskList.map(({ _id, title, category, priority }) => (
+                  <li key={_id}>
+                    <span>
+                      {title} - <b>{category}</b> - <i>{priority}</i>
+                    </span>
+                    <button
+                      className="edit-button"
+                      onClick={() => {
+                        setTask(title);
+                        setCategory(category);
+                        setPriority(priority);
+                        setIsEditing(true);
+                        setCurrentTaskId(_id);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="delete-button"
+                      onClick={() => handleDeleteTask(_id)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No tasks available</p>
+            )}
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
