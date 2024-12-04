f4e2d1b59f0219f5b2edb477a4111fb20f67ba05
Engracia Batista <engraciadacosta@gmail.com>
2024-12-04 16:03:53 -0500

    Commenting App.js and App.css (Not used anymore - need to be cleaned up)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* NOT NEEDED ANYMORE _ CODE MOVED TO TASK.JS
 import React from 'react';
 import './App.css';
 import Footer from './shared/Footer';
@@ -10,7 +11,6 @@ function App() {
         <img src="/letsdologo_header.png" alt="Logo" className="logo" />
         <h2>Task Manager</h2>
         <p>Welcome to LetsDo, your personal task manager.</p>
-        {/* Task Manager Component */}
         <TaskManager />
         <Footer />
       </div>
@@ -19,3 +19,4 @@ function App() {
 }
 
 export default App;
+*/
